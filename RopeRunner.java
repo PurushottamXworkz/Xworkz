11788e7b1fb9a8b81f548a64498dafddcd8d8228
unknown <prashantpattanashetti5@gmail.com>
2024-11-15 18:31:57 +0530

    Runner file of Rope

---
+++ RopeRunner.java
@@ -0,0 +1,101 @@
+class RopeRunner{
+	public static void main(String args[]){
+	Rope rope=new Rope();
+	rope.noOfRopes=10;
+	rope.lengthOfRope=100;
+	rope.nameOfBrand="Nylon";
+	rope.contact=8796451244l;
+	rope.address="Bagalkot";
+	rope.typeOfRole="Large";
+	System.out.println("---------------Object One Rope-----------------");
+	System.out.println("No Of Ropes="+rope.noOfRopes);
+	System.out.println("No Of Ropes="+rope.lengthOfRope);
+	System.out.println("No Of Ropes="+rope.nameOfBrand);
+	System.out.println("No Of Ropes="+rope.contact);
+	System.out.println("No Of Ropes="+rope.address);
+	System.out.println("No Of Ropes="+rope.typeOfRole);
+	rope.lifting();
+	rope.swing();
+	rope.tie();
+	rope.toHang();
+
+	Rope rope1=new Rope();
+	rope1.noOfRopes=50;
+	rope1.lengthOfRope=100;
+	rope1.nameOfBrand="Fiber";
+	rope1.contact=8796451544l;
+	rope1.address="Shibargatti";
+	rope1.typeOfRole="Large";
+	System.out.println("------------Object One Rope1--------------");
+	System.out.println("No Of Ropes="+rope1.noOfRopes);
+	System.out.println("No Of Ropes="+rope1.lengthOfRope);
+	System.out.println("No Of Ropes="+rope1.nameOfBrand);
+	System.out.println("No Of Ropes="+rope1.contact);
+	System.out.println("No Of Ropes="+rope1.address);
+	System.out.println("No Of Ropes="+rope1.typeOfRole);
+	rope1.lifting();
+	rope1.swing();
+	rope1.tie();
+	rope1.toHang();
+
+	Rope rope2=new Rope();
+	rope2.noOfRopes=20;
+	rope2.lengthOfRope=50;
+	rope2.nameOfBrand="Woolen";
+	rope2.contact=8796451555l;
+	rope2.address="Gunji";
+	rope2.typeOfRole="Small";
+	System.out.println("------------Object One Rope2--------------");
+	System.out.println("No Of Ropes="+rope2.noOfRopes);
+	System.out.println("No Of Ropes="+rope2.lengthOfRope);
+	System.out.println("No Of Ropes="+rope2.nameOfBrand);
+	System.out.println("No Of Ropes="+rope2.contact);
+	System.out.println("No Of Ropes="+rope2.address);
+	System.out.println("No Of Ropes="+rope2.typeOfRole);
+	rope2.lifting();
+	rope2.swing();
+	rope2.tie();
+	rope2.toHang();
+
+	Rope rope3=new Rope();
+	rope3.noOfRopes=40;
+	rope3.lengthOfRope=42;
+	rope3.nameOfBrand="Cotton";
+	rope3.contact=8796458544l;
+	rope3.address="Kocchi";
+	rope3.typeOfRole="Small";
+	System.out.println("------------Object One Rope3--------------");
+	System.out.println("No Of Ropes="+rope3.noOfRopes);
+	System.out.println("No Of Ropes="+rope3.lengthOfRope);
+	System.out.println("No Of Ropes="+rope3.nameOfBrand);
+	System.out.println("No Of Ropes="+rope3.contact);
+	System.out.println("No Of Ropes="+rope3.address);
+	System.out.println("No Of Ropes="+rope3.typeOfRole);
+	rope3.lifting();
+	rope3.swing();
+	rope3.tie();
+	rope3.toHang();
+
+	Rope rope4=new Rope();
+	rope4.noOfRopes=48;
+	rope4.lengthOfRope=42;
+	rope4.nameOfBrand="Nylon";
+	rope4.contact=8796458574l;
+	rope4.address="Dharwad";
+	rope4.typeOfRole="Large";
+	System.out.println("------------Object One Rope4--------------");
+	System.out.println("No Of Ropes="+rope4.noOfRopes);
+	System.out.println("No Of Ropes="+rope4.lengthOfRope);
+	System.out.println("No Of Ropes="+rope4.nameOfBrand);
+	System.out.println("No Of Ropes="+rope4.contact);
+	System.out.println("No Of Ropes="+rope4.address);
+	System.out.println("No Of Ropes="+rope4.typeOfRole);
+	rope4.lifting();
+	rope4.swing();
+	rope4.tie();
+	rope4.toHang();
+
+
+
+	}
+}
