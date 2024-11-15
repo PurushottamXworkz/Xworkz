b3c08553bb62aa952c3c5556c5b75250fcbe3413
unknown <prashantpattanashetti5@gmail.com>
2024-11-15 18:31:09 +0530

    class file

---
+++ Rope.java
@@ -0,0 +1,21 @@
+class Rope{
+	int noOfRopes;
+	int lengthOfRope;
+	String nameOfBrand;
+	long contact;
+	String address;
+	String typeOfRole;
+
+	public static void lifting(){
+		System.out.println("To lift the water from well");
+	}
+	public static void swing(){
+		System.out.println("Used to swing");
+	}
+	public static void tie(){
+		System.out.println("Used to tie cow");
+	}
+	public static void toHang(){
+		System.out.println("To hang criminals");
+	}
+}
