1a71f5d64dd35b41affebf7af852a35873605a2a
unknown <prashantpattanashetti5@gmail.com>
2024-11-21 17:01:52 +0530

    String assignment using literals and new keyword

---
+++ Book.java
@@ -0,0 +1,191 @@
+//String task to assign and reassign values
+
+class Book{
+	public static void main(String args[]){
+
+	String bookName="The Book of the Daffodil";
+	String author="William Wordsworth";
+	String ownBy="Sanya Lakhani Podar";
+	String title="The Daffodil";
+	String genre="Historical Fiction";
+	String typeOfBook="Fiction";
+	String address="Mumbai";
+	String language="English";
+
+	System.out.println(bookName);
+	System.out.println(bookName);
+	System.out.println(bookName);
+	System.out.println(bookName);
+	System.out.println(bookName);
+	System.out.println(bookName);
+	System.out.println(bookName);
+	System.out.println(bookName);
+
+
+
+	String bookName1="The Alchemist";
+	String author1="Paulo Coelho";
+	String ownBy1="Paulo Coelho";
+	String title1="The Alchemist";
+	String genre1="Fiction";
+	String typeOfBook1="Fiction";
+	String address1="Mumbai";
+	String language1="Portuguese";
+
+	System.out.println(bookName1);
+	System.out.println(author1);
+	System.out.println(ownBy1);
+	System.out.println(title1);
+	System.out.println(genre1);
+	System.out.println(typeOfBook1);
+	System.out.println(address1);
+	System.out.println(language1);
+
+	System.out.println("Checking Equality of String");
+	System.out.println("----------------------------------");
+	if(bookName==bookName1){
+		System.out.println("true");
+	}
+	else{
+		System.out.println("False");
+	}
+	System.out.println("----------------------------------");
+	System.out.println("----------------------------------");
+	if(author1==author){
+		System.out.println("true");
+	}
+	else{
+		System.out.println("False");
+	}
+	System.out.println("----------------------------------");
+
+	System.out.println("----------------------------------");
+	if(ownBy1==ownBy){
+		System.out.println("true");
+	}
+	else{
+		System.out.println("False");
+	}
+	System.out.println("----------------------------------");
+	System.out.println("----------------------------------");
+	if(title==title1){
+		System.out.println("true");
+	}
+	else{
+		System.out.println("False");
+	}
+	System.out.println("----------------------------------");
+
+
+	String bookName2="Brave New World";
+	String author2="Aldous Huxley";
+	String ownBy2="Thomas and Tomakin";
+	String title2="Brave New World";
+	String genre2="Fiction";
+	String typeOfBook2="Fiction";
+	String address2="Mumbai";
+	String language2="English";
+
+	System.out.println(bookName2);
+	System.out.println(author2);
+	System.out.println(ownBy2);
+	System.out.println(title2);
+	System.out.println(genre2);
+	System.out.println(typeOfBook2);
+	System.out.println(address2);
+	System.out.println(language2);
+
+	String bookName3=new String("To Kill a Mockingbird");
+	String author3=new String ("William Wordsworth");
+	String ownBy3=new String("Sanya Lakhani Podar");
+	String title3=new String("The Daffodil");
+	String genre3=new String("Historical Fiction");
+	String typeOfBook3=new String("Fiction");
+	String address3=new String("Mumbai");
+	String language3=new String("English");
+
+	System.out.println(bookName3);
+	System.out.println(author3);
+	System.out.println(ownBy3);
+	System.out.println(title3);
+	System.out.println(genre3);
+	System.out.println(typeOfBook3);
+	System.out.println(address3);
+	System.out.println(language3);
+
+	String bookName4=new String("Rooted and Remembered");
+	String author4=new String ("C.J. FrederickRooted and Remembered");
+	String ownBy4=new String("C.J. FrederickRooted and Remembered");
+	String title4=new String("Rooted and Remembered");
+	String genre4=new String("Fiction");
+	String typeOfBook4=new String("Fiction");
+	String address4=new String("Mumbai");
+	String language4=new String("Russian");
+
+	System.out.println(bookName4);
+	System.out.println(author4);
+	System.out.println(ownBy4);
+	System.out.println(title4);
+	System.out.println(genre4);
+	System.out.println(typeOfBook4);
+	System.out.println(address4);
+	System.out.println(language4);
+
+	String bookName5=new String("The Lost Bride");
+	String author5=new String ("Nora Roberts");
+	String ownBy5=new String("Nora Roberts");
+	String title5=new String("The Lost Bride");
+	String genre5=new String("Romantic");
+	String typeOfBook5=new String("Romantic");
+	String address5=new String("Mumbai");
+	String language5=new String("English");	
+
+
+	System.out.println(bookName5);
+	System.out.println(author5);
+	System.out.println(ownBy5);
+	System.out.println(title5);
+	System.out.println(genre5);
+	System.out.println(typeOfBook5);
+	System.out.println(address5);
+	System.out.println(language5);
+
+
+	System.out.println("Checking Equality of String");
+	System.out.println("----------------------------------");
+	if(author5==ownBy5){
+		System.out.println("true");
+	}
+	else{
+		System.out.println("False");
+	}
+	System.out.println("----------------------------------");
+	System.out.println("----------------------------------");
+	if(author1==author){
+		System.out.println("true");
+	}
+	else{
+		System.out.println("False");
+	}
+	System.out.println("----------------------------------");
+
+	System.out.println("----------------------------------");
+	if(address5==address4){
+		System.out.println("true");
+	}
+	else{
+		System.out.println("False");
+	}
+	System.out.println("----------------------------------");
+	System.out.println("----------------------------------");
+	if(language5==language4){
+		System.out.println("true");
+	}
+	else{
+		System.out.println("False");
+	}
+	System.out.println("----------------------------------");
+
+
+	}
+}
