8bd701890b763180bc2832b20623401b40e6cc29
unknown <prashantpattanashetti5@gmail.com>
2024-11-27 13:25:20 +0530

    Constructor Chaining using world Object

--- ConstructorChaining/WorldRunner.java
+++ ConstructorChaining/WorldRunner.java
@@ -1,20 +1,20 @@
 class WorldRunner{
     public static void main(String[] args) {
         World w1 = new World();
+        System.out.println("--------------------------------------");
         World w2 = new World("India");
+        System.out.println("--------------------------------------");
         World w3 = new World("India", 29);
-        World w4 = new World("United Kingdom", 29, "London");
-        World w5 = new World("Germany", 29, "Berlin", 100);
-        World w6 = new World("Netherlands", 29, "Amsterdam", 100, "Mark Rutte");
+        System.out.println("--------------------------------------");
+        World w4 = new World("United Kingdom", 4, "London");
+        System.out.println("--------------------------------------");
+        World w5 = new World("Germany", 16, "Berlin", 100);
+        System.out.println("--------------------------------------");
+        World w6 = new World("Netherlands", 12, "Amsterdam", 100, "Mark Rutte");
+        System.out.println("--------------------------------------");
         World w7 = new World("India", 29, "New Delhi", 100, "N Modi", 1400000000);
   
-        // System.out.pritnln(w1);
-        // System.out.pritnln(w2);
-        // System.out.pritnln(w3);
-        // System.out.pritnln(w4);
-        // System.out.pritnln(w5);
-        // System.out.pritnln(w6);
-        // System.out.pritnln(w7);
+      
     }
 
 }
