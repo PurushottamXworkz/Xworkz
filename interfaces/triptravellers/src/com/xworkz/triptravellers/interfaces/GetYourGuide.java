8726d2d2e0808cc61ef3fc14d123ba9fcebca832
unknown <prashantpattanashetti5@gmail.com>
2025-01-29 18:40:13 +0530

    Updated interface task

--- interfaces/triptravellers/src/com/xworkz/triptravellers/interfaces/GetYourGuide.java
+++ interfaces/triptravellers/src/com/xworkz/triptravellers/interfaces/GetYourGuide.java
@@ -1,5 +1,6 @@
 package com.xworkz.triptravellers.interfaces;
 
 public interface GetYourGuide {
-
+	public abstract void tourPackage();
+	public abstract void guideLines();
 }
--- interfaces/triptravellers/src/com/xworkz/triptravellers/interfaces/NammaYaatri.java
+++ interfaces/triptravellers/src/com/xworkz/triptravellers/interfaces/NammaYaatri.java
@@ -1,5 +1,6 @@
 package com.xworkz.triptravellers.interfaces;
 
 public interface NammaYaatri {
-
+	public abstract void tourPackage();
+	public abstract void guideLines();
 }
